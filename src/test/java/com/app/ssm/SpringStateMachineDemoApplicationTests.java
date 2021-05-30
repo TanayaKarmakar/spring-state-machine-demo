196b0c5c91afcf5f7c700c3908826328570cb42e
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-05-30 21:25:32 +0530

    initial setup

---
+++ src/test/java/com/app/ssm/SpringStateMachineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStateMachineDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
